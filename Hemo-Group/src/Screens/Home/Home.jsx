ac2cde55e95f02fd09c666919195fa870f8256aa
TecnoTics <144843516+tecnotics@users.noreply.github.com>
2024-01-26 10:33:03 -0500

    Commit Styles css

--- Hemo-Group/src/Screens/Home/Home.jsx
+++ Hemo-Group/src/Screens/Home/Home.jsx
@@ -1,7 +1,8 @@
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import Navbar from "../../Components/Navbar/Navbar";
 import MoreInfoButton from "../../Components/MoreInfo/MoreInfo";
-import "./Home.css"
+import "./Home.css";
+import logoSolo from "../../assets/navbar-assets/LogoSolo.svg";
 
 function Home() {
   return (
@@ -34,6 +35,95 @@ function Home() {
             <MoreInfoButton />
           </div>
         </section>
+
+        <section className="valores">
+          <Typography variant="h4" component="div" gutterBottom>
+            Nuestros Valores
+          </Typography>
+
+          <Grid container spacing={4} justifyContent="center">
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={logoSolo}
+                  alt="Responsabilidad"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Responsabilidad
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Nos hacemos cargo de las consecuencias de nuestras acciones
+                    y decisiones.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={logoSolo}
+                  alt="Respeto"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Respeto
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Valoramos y consideramos las opiniones y necesidades de los
+                    demás.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={logoSolo}
+                  alt="Integridad"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Integridad
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Actuamos con honestidad y coherencia en todas nuestras
+                    acciones.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </section>
+
+        <section className="contigo"></section>
       </div>
     </>
   );
